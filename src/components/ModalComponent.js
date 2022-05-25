5d08d20c72ed56e21d60c6e083b375dcfd4ed0eb
sharmimr <62257567+sharmimr@users.noreply.github.com>
2022-05-25 18:34:52 +0530

    Update ModalComponent.js

--- src/components/ModalComponent.js
+++ src/components/ModalComponent.js
@@ -1,14 +1,14 @@
 import Modal from 'react-bootstrap/Modal';
 import Button from "react-bootstrap/Button";
 
-const ModalComponent=(props)=>{console.log(props)
+const ModalComponent=(props)=>{
 
     return(
         <Modal show={props.showModal} onHide={props.handleCloseModal}>
         <Modal.Header closeButton>
-            <Modal.Title>Edit Post {props.post.id} {props.post.title}</Modal.Title>
+            <Modal.Title>Edit Post</Modal.Title>
         </Modal.Header>
-        <Modal.Body>{props.post.body}</Modal.Body>
+        <Modal.Body>props.post.body</Modal.Body>
         <Modal.Footer>
             <Button variant="secondary" onClick={props.handleCloseModal}>
             Close
